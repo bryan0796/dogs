18c376b25a256c90e459d21101b8e751baefc554
Bryan de Palma Martins Vida <bpmv96@outlook.com>
2023-07-18 17:36:16 -0300

    feed modal finalizado

--- src/Components/Feed/Feed.jsx
+++ src/Components/Feed/Feed.jsx
@@ -1,14 +1,16 @@
-import React from 'react'
-import FeedPhotos from './FeedPhotos'
-import FeedModal from './FeedModal'
+import React from 'react';
+import FeedPhotos from './FeedPhotos';
+import FeedModal from './FeedModal';
 
 const Feed = () => {
+  const [modalPhoto, setModalPhoto] = React.useState(null);
+
   return (
     <div>
-      <FeedModal />
-      <FeedPhotos />
+      {modalPhoto && <FeedModal photo={modalPhoto}setModalPhoto={setModalPhoto}/>}
+      <FeedPhotos setModalPhoto={setModalPhoto}/>
     </div>
-  )
-}
+  );
+};
 
-export default Feed
+export default Feed;
