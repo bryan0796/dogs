50967be18dcfdc4ec30fa0aedf20293b4e25733b
Bryan de Palma Martins Vida <bpmv96@outlook.com>
2023-07-29 17:00:48 -0300

    adicionado animacao botao home

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,17 +1,42 @@
 import React from 'react';
 import styles from './Header.module.css';
 import { Link } from 'react-router-dom';
-import { ReactComponent as Dogs } from '../Assets/dogs.svg';
+import DogSvg from './DogSvg';
 import { UserContext } from '../UserContext';
 
 const Header = () => {
   const { data } = React.useContext(UserContext);
+  const [olho, setOlho] = React.useState(4);
+
+  function handleDown() {
+    if (olho == 4) {
+      setOlho(0);
+    }
+  }
+
+  function handleUp() {
+    let interval;
+    interval = setInterval(() => {
+      setOlho((olho) => Math.min(olho + 1, 4));
+    }, 160);
+
+    if (olho == 4) clearInterval(interval);
+  }
 
   return (
     <header className={styles.header}>
       <nav className={`${styles.nav} container`}>
-        <Link className={styles.logo} to="/dogs" aria-label="Dogs - Home">
-          <Dogs />
+        <Link
+          style={{ padding: '12px' }}
+          onMouseDown={handleDown}
+          onTouchStart={handleDown}
+          onTouchEnd={handleUp}
+          onMouseUp={handleUp}
+          className={styles.logo}
+          to="/dogs"
+          aria-label="Dogs - Home"
+        >
+          <DogSvg olho={olho} />
         </Link>
         {data ? (
           <Link className={styles.login} to="/conta">
